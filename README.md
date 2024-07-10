61a1e2ef4e9ce0b59ce2e9c062a191f5d7a0717c
tapeau <john_lorenzo_tapia@dlsu.edu.ph>
2024-07-10 20:30:09 +0800

    FIX ASM LOGIC FINALLY

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # RGB to Grayscale
 A simple C and x86 NASM program that converts an RGB image (represented as a three-dimensional array of values ranging from 0-255) into its grayscale equivalent (represented as a two-dimensional array). Developed for DLSU LBYARCH course (T3 2023-2024).
 
-## Compiling the program
-Run the file `compile.bat`
+## Running the program
+1. Run the file `compile.bat`
+2. Run the program EXE through command line to see the output
--- cvtRGBToGray.asm
+++ cvtRGBToGray.asm
@@ -2,32 +2,33 @@ section .text
 global cvtRGBToGray
 
 cvtRGBToGray:
-    mov r10, r8     ; Load m into R10 for outer loop (rows)
-
-; Outer loop (rows)
-outer_loop:
-    mov r11, r9     ; Load n into R11 for inner loop (columns)
-
-; Inner loop (columns)
-; TODO: Fix whatever the fuck is wrong with it
-inner_loop:
-    movzx rax, byte [rdx]     ; Move R from img1[i][j][0] into RAX
-    movzx rbx, byte [rdx+1]   ; Move G from img1[i][j][1] into RBX
-    add rbx, rax              ; RAX = RAX + RBX (R + G)
-    movzx rbx, byte [rdx+2]   ; Move B from img1[i][j][2] into RBX
-    add rax, rbx              ; RAX = R + G + B
-    mov rbx, 0x3              ; RBX = 3
-    div rbx                   ; RAX / RBX (grayscale)
-    mov [rcx], rax            ; Store grayscale value in img2[i][j]
-
-    add rdx, 0x3              ; Move img1 pointer to next pixel
-    add rcx, 0x1              ; Move img2 pointer to next pixel
-
-    dec r11                   ; Decrement inner loop counter
-    jnz inner_loop            ; Jump to inner_loop if R11 != 0
-
-    dec r10                   ; Decrement outer loop counter
-    jnz outer_loop            ; Jump to outer_loop if R10 != 0
-
+    mov rsi, rdx          ; rsi = rcx (img1 pointer)
+    mov rdi, rcx          ; rdi = rdx (img2 pointer)
+    xor rax, rax          ; Clear RAX for grayscale calculation
+    
+    ; Outer loop (rows)
+    outer_loop:
+        mov rdx, r9       ; RDX = n (inner loop counter)
+        
+        ; Inner loop (columns)
+        inner_loop:
+            movzx eax, byte [rsi]        ; EAX = R (img1[i][j][0])
+            movzx ebx, byte [rsi + 1]    ; EBX = G (img1[i][j][1])
+            add eax, ebx                 ; EAX = R + G
+            movzx ebx, byte [rsi + 2]    ; EBX = B (img1[i][j][2])
+            add eax, ebx                 ; EAX = R + G + B
+            mov bl, 3                    ; BL = 3
+            div bl                       ; EAX / BL (grayscale)
+            mov [rdi], al                ; Store grayscale value in img2[i][j]
+            
+            add rsi, 3                   ; Move img1 pointer to next pixel
+            inc rdi                      ; Move img2 pointer to next pixel
+            
+            dec edx                      ; Decrement inner loop counter
+            jnz inner_loop               ; Continue inner loop if EDX != 0
+        
+        dec r8                           ; Decrement outer loop counter
+        jnz outer_loop                   ; Continue outer loop if R8 != 0
+    
     ; Return to C program
     ret
